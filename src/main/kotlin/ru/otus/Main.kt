d6668415d3b541ef4810c4cac0cedb6a2b3045fd
luminorena <94684347+luminorena@users.noreply.github.com>
2026-05-24 20:19:37 +0300

    added 1st task

--- src/main/kotlin/ru/otus/Main.kt
+++ src/main/kotlin/ru/otus/Main.kt
@@ -1,16 +1,48 @@
-package ru.otus.ru.otus
+package ru.otus
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/**
+ * ДЗ #1
+ *
+ *    1. Программа должна вывести:
+ *       а) Строку приветствия пользователя библиотеки
+ *       б) Карточку первой книги в указанном формате
+ *    2. Использовать правильные типы для каждого поля
+ *       (цена — Double, год и количество — Int).
+ *    3. ★ Бонус (опционально): функция showLiterals()
+ *       с экспериментами по числовым нотациям.
+ */
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val user: String = "Olga"
+    val bookName: String = "Над пропастью во ржи"
+    val author: String = "Джером Дэвид Сэлинджер"
+    val originalName: String = "The Catcher in the Rye"
+    val genre: String = "Роман"
+    val sourceLanguage: String = "английский"
+    val publisher: String = "Little, Brown and Company"
+    val pages: Int = 213
+    val isbn: String = "978-5-699-30534-6"
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    println("Добро пожаловать в библиотеку $user!")
+    println("Название книги: $bookName")
+    println("Автор: $author")
+    println("Оригинальное название: $originalName")
+    println("Жанр: $genre")
+    println("Язык оригинала: $sourceLanguage")
+    println("Издательство: $publisher")
+    println("Количество страниц: $pages")
+    println("ISBN: $isbn")
+
+    println(showLiterals())
+}
+
+fun showLiterals() {
+    val intNumber: Int = 1_000_000
+    val hexNumber: Int = 0xCAFE
+    val binaryNumber: Int = 0b1010_1010
+    val doubleNumber: Double = 1.5e3
+
+    println("Миллион с подчёркиваниями: $intNumber")
+    println("Десятичное представление hex: $hexNumber")
+    println("Десятичное представление binary: $binaryNumber")
+    println("Стандартная запись дробного числа: $doubleNumber")
 }
